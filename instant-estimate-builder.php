f47b33806e19b1113af8c2cada2b78873ed55136
Jack TMG <jack@jack-tmg.local>
2026-07-03 09:31:05 -0500

    fix: use packaged release assets

--- instant-estimate-builder.php
+++ instant-estimate-builder.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Instant Estimate Builder
  * Description: Build customizable multi-step instant estimate forms for local service businesses with lead capture, notifications, and lead management.
- * Version: 1.0.11
+ * Version: 1.0.12
  * Requires at least: 5.4
  * Tested up to: 7.0
  * Requires PHP: 7.4
@@ -25,6 +25,8 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
     __FILE__,
     'instant-estimate-builder'
 );
+
+$myUpdateChecker->getVcsApi()->enableReleaseAssets('/instant-estimate-builder\.zip$/i');
 
 $myUpdateChecker->addFilter('request_update_result', function ($update) {
     if (empty($update)) {
@@ -620,7 +622,7 @@ add_filter('plugins_api', function ($result, $action, $args) {
     return (object) [
         'name'           => 'Instant Estimate Builder',
         'slug'           => 'instant-estimate-builder',
-        'version'        => '1.0.11',
+        'version'        => '1.0.12',
         'author'         => '<a href="https://taggartmediagroup.com/">Taggart Media Group</a>',
         'author_profile' => 'https://taggartmediagroup.com/',
         'homepage'       => 'https://taggartmediagroup.com/',
@@ -647,6 +649,8 @@ add_filter('plugins_api', function ($result, $action, $args) {
                 </ul>
             ',
             'changelog' => '
+                <h4>1.0.12</h4>
+                <ul><li>Use packaged release ZIP assets for cleaner WordPress updates.</li></ul>
                 <h4>1.0.11</h4>
                 <ul><li>Updated plugin update metadata and branded icon assets.</li></ul>
                 <h4>1.0.10</h4>
